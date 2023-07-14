c0a872b87e219c7c208a3c5bc26f862daf75af2b
Aubrey <ragandanaubrey@gmail.com>
2023-07-14 20:34:14 +0900

    Answer the assignments

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,27 @@
+// LECTURE: Values and Variables
+const country = "Philippines";
+const continent = "Asian Continent";
+let population = 109840000;
+console.log(country, continent, population);
+
+// LECTURE: Data Types
+let isIsland = 7640;
+let language;
+console.log(isIsland, population, country, language);
+
+// LECTURE: let, const and var
+language = "Tagalog";
+// continent = "Europe";
+
+// LECTURE: Basic Operators
+const eachHalf = population / 2;
+console.log(eachHalf);
+population++;
+let finland = 6000000;
+console.log(population > finland);
+let averagePopulation = 33000000;
+console.log(country < averagePopulation);
+console.log(population);
+let description =
+  "Philippines is in Asia, and its 109.84 million people speaks Tagalog";
+console.log(description);
