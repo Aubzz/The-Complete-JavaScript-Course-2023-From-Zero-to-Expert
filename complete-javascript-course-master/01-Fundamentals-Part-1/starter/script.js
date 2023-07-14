4f4bfd124f206e3f5d753fd756eed9411073edd0
Aubrey <ragandanaubrey@gmail.com>
2023-07-14 19:34:33 +0900

    Add a folder from Jonas GitHub

---
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,92 @@
+/* let js = "amazing";
+if (js === "amazing") alert("JavaScript is so fun!");
+
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let years = "3";
+console.log(jsa);
+
+true;
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 19);
+console.log(typeof "Abby");
+
+javascriptIsFun = "Yes";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age = 25;
+age = 19;
+console.log(age);
+
+const birthYear = 1991;
+birthYear = 1990;
+
+var job = "Software Engineer";
+job = "farmer";
+console.log(job);
+
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarha = now - 2003;
+console.log(ageJonas, ageSarha);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+//  2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Aubrey";
+const lastName = "Ragandan";
+console.log(firstName + " " + lastName);
+
+// Assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarha);
+console.log(ageSarha >= 18);
+
+const isFullAge = ageSarha >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarha = now - 2018;
+
+console.log(now - 1991 > now - 2018);
+
+// console.log(30 - 10 - 15);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+// console.log(x, y);
+
+const averageAge = (ageJonas + ageSarha) / 2;
+console.log(ageJonas, ageSarha, averageAge);
