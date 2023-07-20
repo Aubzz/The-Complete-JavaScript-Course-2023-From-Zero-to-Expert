0406ca8846b9e0bbf41b1fd091492819d2e3c13a
Aubrey <ragandanaubrey@gmail.com>
2023-07-20 12:16:08 +0900

    Add logical operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -192,7 +192,7 @@ if (height) {
 } else {
   console.log("Height is UNDEFINED");
 }
-*/
+
 const age = "18";
 if (age === 18) console.log("You just became an adult :D (strict)");
 
@@ -212,3 +212,28 @@ if (favorite == 23) {
 }
 
 if (favorite !== 23) console.log("Why not 23?");
+*/
+
+// Logical Operators
+const hasDriverLicense = true; // A
+const hasGoodVision = true; // B
+console.log(hasDriverLicense && hasGoodVision);
+console.log(hasDriverLicense || hasGoodVision);
+console.log(!hasDriverLicense);
+
+// const shouldDrive = hasDriverLicense && hasGoodVision;
+
+// if (hasDriverLicense && hasGoodVision) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive");
+// }
+
+const isTired = false; // C
+console.log(hasDriverLicense && hasGoodVision && isTired);
+
+if (hasDriverLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive");
+}
