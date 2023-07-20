987ce88672912e12b161e7fcda08610d15a00594
Aubrey <ragandanaubrey@gmail.com>
2023-07-20 20:00:40 +0900

    Tweaks some statement

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -237,47 +237,17 @@ if (hasDriverLicense && hasGoodVision && !isTired) {
 } else {
   console.log("Someone else should drive");
 }
+
+// statements and expression
+if (23 > 0) {
+  const str = "23 is bigger";
+}
+
+const me = "Jonas";
+console.log(`I'm ${2037 - 1991} years old ${me}`);
 */
 
-const day = "monday";
-
-switch (day) {
-  case "monday": // day === "monday"
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
-    break;
-  case "tuesday":
-    console.log("Prepare theory videos");
-    break;
-  case "wednesday":
-  case "thursday":
-    console.log("Write code examples");
-    break;
-  case "friday":
-    console.log("Record videos");
-    bconsole.log("Record videos");
-    reak;
-  case "saturday":
-  case "sunday":
-    console.log("Enjoy the weekend");
-    break;
-  default:
-    console.log("Not a valid day!");
-}
-
-const day = "thursday";
-
-if (day === "monday") {
-  console.log("Plan course structure");
-  console.log("Go to coding meetup");
-} else if (day === "tuesday") {
-  console.log("Prepare theory videos");
-} else if (day === "wednesday" || day === "thursday") {
-  console.log("Write code examples");
-} else if (day === "friday") {
-  console.log("Record videos");
-} else if (day === "saturday" || day === "sunday") {
-  console.log("Enjoy the weekend");
-} else {
-  console.log("Not a valid day!");
-}
+const age = 15;
+age >= 18
+  ? console.log("I like to drink wine 🍷")
+  : console.log("I like to drink water 💧");
