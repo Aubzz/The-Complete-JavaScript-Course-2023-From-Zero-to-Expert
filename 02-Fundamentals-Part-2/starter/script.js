91cf79c9a23782f6a7a2d4765aa14cf588c2f59e
Aubrey <ragandanaubrey@gmail.com>
2023-07-23 20:37:10 +0900

    Add function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -11,3 +12,22 @@ const interface = "Audio";
 const private = 541;
 const break = 23;
 const if = 3; 
+*/
+
+function logger() {
+  console.log("My name is Aubrey");
+}
+
+// calling / running / invoking functions
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
