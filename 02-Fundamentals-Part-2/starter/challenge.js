e429be12b3815a8f4c7482d21077d6ee0e734aab
Aubrey <ragandanaubrey@gmail.com>
2023-07-30 18:29:55 +0900

    Little tweaks

--- 02-Fundamentals-Part-2/starter/challenge.js
+++ 02-Fundamentals-Part-2/starter/challenge.js
@@ -1,26 +1,18 @@
-const calcAverage = (
-  dolphinTeam1,
-  dolphinTeam2,
-  dolphinTeam3,
-  koalasTeam1,
-  koalasTeam2,
-  KoalasTeam3
-) => {
-  const avgDolphin =
-    calcAverage(dolphinTeam1 + dolphinTeam2 + dolphinTeam3) / 3;
-  const avgKoalas = calcAverage(koalasTeam1 + koalasTeam2 + KoalasTeam3) / 3;
-};
-console.log(calcAverage(44, 23, 71, 65, 54, 49));
+const calcAverage = (team1, team2, team3) => (team1 + team2 + team3) / 3;
+console.log(calcAverage(3, 4, 5));
+
+const scoresDolphin = calcAverage(44, 23, 71);
+const scoresKoalas = calcAverage(65, 54, 49);
+console.log(scoresDolphin, scoresKoalas);
 
 const checkWinner = function (avgDolphin, avgKoalas) {
-  calcAverage();
-  if (avgDolphin > avgKoalas && dolphinTeam1 >= 2 * koalasTeam1) {
-    console.log(`Dolphin win (${dolphinTeam1} vs ${koalasTeam1})`);
-  } else if (avgKoalas > avgDolphin && dolphinTeam2 >= 2 * koalasTeam2) {
-    console.log(`Koalas win ${dolphinTeam2} vs ${koalasTeam2}`);
+  if (avgDolphin >= 2 * avgKoalas) {
+    console.log(`Dolphin win (${avgDolphin} vs ${avgKoalas}})`);
+  } else if (avgKoalas >= 2 * avgDolphin) {
+    console.log(`Koalas win ${avgKoalas} vs ${avgDolphin}`);
   } else {
     console.log("No team wins!");
   }
 };
 
-console.log(checkWinner());
+checkWinner(scoresDolphin, scoresKoalas);
